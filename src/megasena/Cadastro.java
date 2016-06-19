5d948e5a3c1c4a1e75765064c28289af10ba7de7
Patrick Porto <patrick.porto@concretesolutions.com.br>
2016-06-19 00:54:25 -0300

    Leitura de apostadores

--- src/megasena/Cadastro.java
+++ src/megasena/Cadastro.java
@@ -1,15 +1,32 @@
 
 package megasena;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
 /*
  * A classe Cadastro encapsula o conjunto de todos os apostadores cadastrados.
  */
 public class Cadastro {
+    private HashMap<String, Apostador> apostadores = new HashMap<>();
     /*
      *  Construtor.
      */
     public Cadastro (String nomeArqCadatro) {
-        // Completar...
+        try {
+            FileReader arq = new FileReader(nomeArqCadatro);
+            BufferedReader lerArq = new BufferedReader(arq);
+            String linha = lerArq.readLine();
+            while (linha != null) {
+                Apostador apostador = new Apostador();
+                apostador.setCPF(linha.substring(0, 11));
+                apostador.setNome(linha.substring(12));
+                this.apostadores.put(apostador.getCPF(), apostador);
+                linha = lerArq.readLine();
+            }
+        } catch (Exception e) {
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
     }
-    
 }
