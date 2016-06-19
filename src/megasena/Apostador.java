9d54db2df8d2b602cb44ccbd405f25aec4a280c8
Patrick Porto <patrick.porto@concretesolutions.com.br>
2016-06-19 00:30:20 -0300

    Criação de classe apostador

---
+++ src/megasena/Apostador.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package megasena;
+
+/**
+ *
+ * @author patrick.porto
+ */
+public class Apostador {
+    private String nome;
+    private String cpf;
+    
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+}
